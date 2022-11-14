94473c17ce0741173fdaa8d82010ce68bb5d2cb3
Sabya Sachi <sabyasachi@Sabyas-MacBook-Pro-2.local>
2022-11-14 11:05:38 +0530

    removed the firebase configs

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import {  getAuth, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyAtUR5ZtxooTSe4kFfsA4OI2Vade9PRHAk",
-    authDomain: "fir-crud-9136f.firebaseapp.com",
-    databaseURL: "https://fir-crud-9136f.firebaseio.com",
-    projectId: "fir-crud-9136f",
-    storageBucket: "fir-crud-9136f.appspot.com",
-    messagingSenderId: "249606379775",
-    appId: "1:249606379775:web:8a7ebbc1d620e924"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 }
 
 const app = initializeApp(firebaseConfig);
